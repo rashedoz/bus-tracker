a397c5ea34aacbb145cc4935b55f8bfb7b7f0c24
Rashed Mazumder <yaan.uchiha29@gmail.com>
2018-08-02 00:22:48 +0600

    Added All modules

--- Bus_Project_299/Bus_Project_299.ino
+++ Bus_Project_299/Bus_Project_299.ino
@@ -32,199 +32,318 @@
  
  */
  /* The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
+ * LCD RS pin to digital pin 6
+ * LCD Enable pin to digital pin 7
+ * LCD D4 pin to digital pin 14 (A0)
+ * LCD D5 pin to digital pin 15
+ * LCD D6 pin to digital pin 16
+ * LCD D7 pin to digital pin 17
  * LCD R/W pin to ground
  * LCD VSS pin to ground
  * LCD VCC pin to 5V
  * 10K resistor:
  * ends to +5V and ground
  * wiper to LCD VO pin (pin 3) */
+
+ /* GPS | Arduinoo
+  *  TX | 5
+  *  RX | 4
+  *  VCC | 3.3V
+ */
+ 
  
 #include <SPI.h>
 #include <MFRC522.h>
 #include <LiquidCrystal.h>
-#include <SoftwareSerial.h> 
+#include <SoftwareSerial.h>
+#include <TinyGPS++.h> 
 
+//RFID
 #define RST_PIN         9           // Configurable, see typical pin layout above
 #define SS_PIN          10          // Configurable, see typical pin layout above
 
 //Wifi pin ESP8266
-#define RX 2 
-#define TX 3
+#define RX 2 //2
+#define TX 3 //3
+
+//GPS Pins
+static const int RXPin = 5, TXPin = 4;
+static const uint32_t GPSBaud = 9600;
+
+TinyGPSPlus gps;  // The TinyGPS++ object
+
 
 // LCD Config
-const int rs = 6, en = 7, d4 = 5, d5 = 4, d6 = 14, d7 = 15;
+const int rs = 6, en = 7, d4 = 14, d5 = 15, d6 = 16, d7 = 17;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
 
-//Config ESP8266 Wifi Module
+
+/*            ****Config ESP8266 Wifi Module****            */
+
 String AP = "Walankaa";       // CHANGE ME
 String PASS = "tokyoghoul"; // CHANGE ME
-String API = "FDW7ERYQ1EM9EXRZ";   // CHANGE ME
+String API = "RPI7NWXKPYQXEUTR";   // CHANGE Write API Of Channel
 String HOST = "api.thingspeak.com";
 String PORT = "80";
-String field = "field1";    //Thingspeak Filed Name
+String field1 = "field1";   //lattitude
+String field2 = "field2";   //longitude
 int countTrueCommand;
 int countTimeCommand; 
 boolean found = false; 
-int valSensor = 1;
 
+int sendCounter=1;
+
+
+//Passenger List
 int uid_counter=0;
 String passenger_list[20];
 
 
-SoftwareSerial esp8266(RX,TX); 
+SoftwareSerial esp8266(RX,TX);  //Wifi Module Serial
+SoftwareSerial ss(RXPin, TXPin);  // The serial connection to the GPS device
 
-void setup() {
-  Serial.begin(9600);        // Initialize serial communications with the PC
-  esp8266.begin(9600);       ///wifi serial
-  SPI.begin();               // Init SPI bus
-  mfrc522.PCD_Init();        // Init MFRC522 card
+    void setup() {
+                Serial.begin(9600);        // Initialize serial communications with the PC
+                
+                SPI.begin();               // Init SPI bus
+                mfrc522.PCD_Init();        // Init MFRC522 card
+                ss.begin(GPSBaud);         //GPS 
+                
+                lcd.print("Starting System");
+             
+                
+                Serial.println(F(" Bus System Ready! "));
+                
+                // set up the LCD's number of columns and rows:
+                lcd.begin(16, 2);
+                lcd.setCursor(0,0);
+                lcd.print("Bus Status!");
   
-  Serial.println(F("Write personal data on a MIFARE PICC "));
-  
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-  lcd.print("Bus Status!");
-}
+    }
 
-void loop() {
+      void loop() {
 
-  // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
-  MFRC522::MIFARE_Key key;
-  for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
+                    /*Get Gps Data in variable*/
+                    double longitude;
+                    double lattitude;
+                    double *l;
+                    double *ltt;
+                    l = &longitude;
+                    ltt = &lattitude;
+                    getGPS(l,ltt); //Get GPS Location
 
-  // Look for new cards
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {
-    return;
-  }
+                    Serial.print("Longitude:");
+                    Serial.print(longitude,6);
+                    Serial.print("Lattitude:");
+                    Serial.println(lattitude,6);
 
-  // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) {
-    return;
-  }
+                    if(sendCounter%100==0){
+                          ss.end();
+                          
+                          esp8266.begin(9600);       ///wifi serial
+                          
+                        //Setup Wifi Module
+                        sendCommand("AT",5,"OK");
+                        sendCommand("AT+CWMODE=1",5,"OK");
+                        sendCommand("AT+CWJAP=\""+ AP +"\",\""+ PASS +"\"",20,"OK");
   
-  /*Dump card Info*/
-  Serial.print(F("Card UID:"));    //Dump UID
-  byte uid;
-  byte suid;
-  String sid;
-  String full_id="";
-  for (byte i = 0; i < mfrc522.uid.size; i++) {
-     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-     Serial.print(mfrc522.uid.uidByte[i], HEX);
-     uid = mfrc522.uid.uidByte[i];
-     Serial.print("(");
-     Serial.print(uid);
-     Serial.print(")");
-     sid = String(uid); //Conver UID to string for complete UID
-     full_id =full_id + sid;
-   
-  }
+                          
+                          lcd.print("Sending GPS");
+                          Serial.print("SendCounter:");
+                          Serial.println(sendCounter);
+                          sendData(lattitude,longitude);
+                          lcd.clear();
+                          lcd.print("Sent");
+                          lcd.clear();
   
-  /*unsigned long final_uid=full_id.toInt();
-  Serial.print("\nUID:");
-  Serial.print(final_uid);
-  Serial.print("\nScanned UID:");
-  Serial.print(full_id);*/
-  
-  for(int i=0;i<=uid_counter;i++){
-      
-      if (full_id==passenger_list[i]){
-        Serial.println("Checked  In!!!!");
-       
-        break;
+                          
+                          esp8266.end();
+                          ss.begin(GPSBaud);         //GPS
+                      }
+                      
+                  sendCounter++;
+                  
+                  // Prepare key - all keys are set to FFFFFFFFFFFFh at chip delivery from the factory.
+                  MFRC522::MIFARE_Key key;
+                  for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
+                
+                  // Look for new cards
+                  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+                    return;
+                  }
+                
+                  // Select one of the cards
+                  if ( ! mfrc522.PICC_ReadCardSerial()) {
+                    return;
+                  }
+                  
+                  /*Dump card Info and Convert UID to String*/
+                  Serial.print(F("Card UID:"));    //Dump UID
+                  byte uid;
+                  byte suid;
+                  String sid;
+                  String full_id="";
+                  for (byte i = 0; i < mfrc522.uid.size; i++) {
+                     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+                     //Serial.print(mfrc522.uid.uidByte[i], HEX);
+                     uid = mfrc522.uid.uidByte[i];
+                     //Serial.print("(");
+                     Serial.print(uid);
+                    // Serial.print(")");
+                     sid = String(uid); //Conver UID to string for complete UID
+                     full_id =full_id + sid;
+                   
+                  }
+                  
+                  /*unsigned long final_uid=full_id.toInt();
+                  Serial.print("\nUID:");
+                  Serial.print(final_uid);
+                  Serial.print("\nScanned UID:");
+                  Serial.print(full_id);*/
+                
+                 
+                
+                  
+                  /*Card Info*/
+                  //Serial.print(F(" PICC type: "));   // Dump PICC type
+                  MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
+                  //Serial.println(mfrc522.PICC_GetTypeName(piccType));
+                
+                  /* Sector with keys: 3,7,11,15,19,23,27,31,35,39,43,47,51,55,59,63(DO NOT TOUCH)*/
+                  MFRC522::StatusCode status;
+                
+                 /*                          *******Write Data and Balance to RFID*************          */
+                 
+                  /* Sector with keys: 3,7,11,15,19,23,27,31,35,39,43,47,51,55,59,63(DO NOT TOUCH)/
+                  Serial.println(F("Type Your name, ending with #")); //Store Passenger Name
+                  writeData(12,13,key); //write name   */
+                  
+                  /* UNCOMMENT FOR READINGH SECTOR
+                  Serial.println("Enter sector to print");
+                  int sectorToRead=Serial.parseInt(); //Serial Read sector to read
+                  Serial.println(sectorToRead);   
+                  dumpSector(sectorToRead,key); */
+                  
+                  /* Sector with keys: 3,7,11,15,19,23,27,31,35,39,43,47,51,55,59,63(DO NOT TOUCH)/
+                  //Enter User Balance
+                  Serial.println("Enter Balace: ");
+                  writeData(16,17,key); //Enter Balance */
+                
+                  
+                  //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));      // see all blocks in hex
+                  
+                
+                  /*          ********************Read Name and Balance from RFID Card***************       */
+                  
+                  String n;
+                  String b;
+                  
+                  Serial.print(F(" Name: "));
+                  
+                  n = readData(12,key);
+                  Serial.print(n);
+                
+                  Serial.print(F(" Balance: "));
+                  b = readData(16,key);
+                  int balance;
+                  balance = b.toInt();  //convert balance to int
+                  Serial.print(balance);
+                  
+                
+                  
+                
+                  /*lcd.setCursor(0,1);
+                  lcd.print("Passenger:");
+                  lcd.print(uid_counter);*/
+                
+                                                           /*Check IN and Check Out of RFID */                                    
+                  for(int i=0;i<=uid_counter;i++){
+                      if (full_id==passenger_list[i]){
+                        Serial.println("  Checked  Out");
+                        lcd.clear();
+                        lcd.setCursor(0,0);
+                        lcd.print("Checked  Out!!");
+                        lcd.setCursor(0,1);
+                        lcd.print(n);
+                        lcd.setCursor(8,1);
+                        lcd.print(":");
+                        lcd.print(balance);
+                        delay(900);
+                        lcd.clear();
+                        passenger_list[i] = passenger_list[uid_counter-1];
+                        passenger_list[uid_counter-1] = "";
+                        uid_counter--;
+                        
+                        break;
+                      }
+                      else if(full_id!=passenger_list[i] && (i==uid_counter || uid_counter==0)){
+                        //Serial.print(full_id);
+                        Serial.print("  Check In");
+                        lcd.clear();
+                        lcd.setCursor(0,0);
+                        lcd.print("Checked In!!");
+                        lcd.setCursor(0,1);
+                        lcd.print(n);
+                        lcd.setCursor(8,1);
+                        lcd.print(":");
+                        lcd.print(balance);
+                        delay(900);
+                        lcd.clear();
+                        passenger_list[uid_counter] = full_id;
+                        uid_counter++;
+                        break;
+                        }  
+                  }
+                
+                 //Print Current Passenger List
+                   Serial.print("\n Current Passenger:");
+                   Serial.println(uid_counter);
+                   for(int i=0;i<uid_counter;i++){
+                      Serial.print("Passenger[");
+                      Serial.print(i);
+                      Serial.print("]:");
+                      Serial.println(passenger_list[i]);
+                
+                   }
+                  Serial.print("\n");
+                
+                  //lcd.clear();
+                  lcd.setCursor(0,0);
+                  lcd.print("Passenger:");
+                  lcd.print(uid_counter);
+                
+                  
+                  
+                  //Serial.println(" ");
+                  mfrc522.PICC_HaltA(); // Halt PICC
+                  mfrc522.PCD_StopCrypto1();  // Stop encryption on PCD
+                
       }
-      else if(full_id!=passenger_list[i] && (i==uid_counter || uid_counter==0)){
-        //Serial.print(full_id);
-        Serial.print("Check In");
-        passenger_list[uid_counter] = full_id;
-        uid_counter++;
-        break;
-        }  
-  }
-   
-   Serial.print("\n Current Passenger:");
-   Serial.println(uid_counter);
-   for(int i=0;i<uid_counter;i++){
-      Serial.print("Passenger[");
-      Serial.print(i);
-      Serial.print("]:");
-      Serial.println(passenger_list[i]);
 
-   }
 
-  
-  
 
-  
-  
 
-  
- 
-   
-  //Serial.print(F(" PICC type: "));   // Dump PICC type
-  MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
-  //Serial.println(mfrc522.PICC_GetTypeName(piccType));
 
-  /* Sector with keys: 3,7,11,15,19,23,27,31,35,39,43,47,51,55,59,63(DO NOT TOUCH)*/
-  MFRC522::StatusCode status;
 
-  /* Sector with keys: 3,7,11,15,19,23,27,31,35,39,43,47,51,55,59,63(DO NOT TOUCH)/
-  Serial.println(F("Type Your name, ending with #")); //Store Passenger Name
-  writeData(12,13,key); //write name   */
-  
-  /* UNCOMMENT FOR READINGH SECTOR
-  Serial.println("Enter sector to print");
-  int sectorToRead=Serial.parseInt(); //Serial Read sector to read
-  Serial.println(sectorToRead);   
-  dumpSector(sectorToRead,key); */
-  
-  /* Sector with keys: 3,7,11,15,19,23,27,31,35,39,43,47,51,55,59,63(DO NOT TOUCH)/
-  //Enter User Balance
-  Serial.println("Enter Balace: ");
-  writeData(16,17,key); //Enter Balance */
 
-  
-  //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));      //uncomment this to see all blocks in hex
 
-  String n;
-  String b;
-  
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Name: ");
-  //Serial.print(F("Name: "));
-  
-  n = readData(12,key);
- // Serial.print(n);
-  lcd.print(n);
 
- 
-  lcd.setCursor(0,1);
-  lcd.print("Balance: ");
-  //Serial.print(F("Balance: "));
-  b = readData(16,key);
-  int balance;
-  balance = b.toInt();  //convert balance to int
-  //Serial.print(balance);
-  lcd.print(balance);
-  
-  
-  //Serial.println(" ");
-  mfrc522.PICC_HaltA(); // Halt PICC
-  mfrc522.PCD_StopCrypto1();  // Stop encryption on PCD
 
-}
+
+
+void setupWifi(){
+
+                  //Setup Wifi Module
+                  sendCommand("AT",5,"OK");
+                  sendCommand("AT+CWMODE=1",5,"OK");
+                  sendCommand("AT+CWJAP=\""+ AP +"\",\""+ PASS +"\"",20,"OK");
+ }
 
 
 /*Write Data to RFID Sector*/
+
 void writeData(int b1, int b2,MFRC522::MIFARE_Key key){ 
         key=key;
         byte buffer[34];
@@ -276,6 +395,8 @@ void writeData(int b1, int b2,MFRC522::MIFARE_Key key){
         else Serial.println(F("MIFARE_Write() success:"));
    }
 
+
+
   /*Dump Sector Data*/
   void dumpSector(int sector,MFRC522::MIFARE_Key key){
     
@@ -285,6 +406,8 @@ void writeData(int b1, int b2,MFRC522::MIFARE_Key key){
     Serial.println();
     
     }
+
+
 
     
    /*Read Data*/
@@ -325,3 +448,63 @@ void writeData(int b1, int b2,MFRC522::MIFARE_Key key){
         //Serial.print(" ");
         return sfull;
     }
+
+
+/*    ***Send GPS Data to ThingsSpeak***   */
+void sendData(double lattitude,double longitude){
+    
+     String getData = "GET /update?api_key="+ API +"&"+ field1 +"=" + lattitude +"&"+ field2 +"=" + longitude;
+     sendCommand("AT+CIPMUX=1",5,"OK");
+     sendCommand("AT+CIPSTART=0,\"TCP\",\""+ HOST +"\","+ PORT,15,"OK");
+     sendCommand("AT+CIPSEND=0," +String(getData.length()+4),4,">");
+     esp8266.println(getData);delay(1500);countTrueCommand++;
+     sendCommand("AT+CIPCLOSE=0",5,"OK");
+}
+
+/*    ***Send Command to Wifi Module***   */
+ void sendCommand(String command, int maxTime, char readReplay[]) {
+      Serial.print(countTrueCommand);
+      Serial.print(". at command => ");
+      Serial.print(command);
+      Serial.print(" ");
+      while(countTimeCommand < (maxTime*1))
+      {
+        esp8266.println(command);//at+cipsend
+        if(esp8266.find(readReplay))//ok
+        {
+          found = true;
+          break;
+        }
+      
+        countTimeCommand++;
+      }
+      
+      if(found == true)
+      {
+        Serial.println("Ok");
+        countTrueCommand++;
+        countTimeCommand = 0;
+      }
+      
+      if(found == false)
+      {
+        Serial.println("Fail");
+        countTrueCommand = 0;
+        countTimeCommand = 0;
+      }
+      
+      found = false;
+ }
+
+  /* ***** GET GPS Location ******* */
+ void getGPS(double *longitude,double *lattitude){
+  
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (ss.available() > 0){
+    gps.encode(ss.read());
+    if (gps.location.isUpdated()){
+      *lattitude = gps.location.lat();
+      *longitude = gps.location.lng(); 
+    }
+  }
+ }
